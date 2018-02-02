d566477b57662e0d6a8a9ef8b70c9aa4f17916e9
SidneyNemzer <sidney@ucode.com>
2018-02-01 17:57:58 -0800

    Replace expandCallbackObject with schema

--- src/js/schema.js
+++ src/js/schema.js
@@ -1,5 +1,5 @@
 import * as R from 'ramda'
-import { callback } from './constants'
+import * as constants from './constants'
 import * as defaults from './defaults'
 
 const isType = (expected, value) => (
@@ -42,50 +42,31 @@ export const createSchema = schemaDescription => value => {
   )
 }
 
-export const expandCallbackObject = (logger, portName, portDefinitionCallback) => {
-  let expandedCallback = false
-  if (typeof portDefinitionCallback === 'object') {
-    if (
-      portDefinitionCallback.tag !== undefined
-      && typeof portDefinitionCallback.tag !== 'function'
-      && typeof portDefinitionCallback.tag !== 'boolean'
-    ) {
-      throw new TypeError(`Port ${portName}.callback.tag should be a function but it's ${typeof portDefinitionCallback.tag}`)
-    }
-
-    if (
-      portDefinitionCallback.name !== undefined
-      && typeof portDefinitionCallback.name !== 'string'
-    ) {
-      throw new TypeError(`Port ${portName}.callback.name should be a string but it's ${typeof portDefinitionCallback.name}`)
-    }
-
-    expandedCallback = {
-      type: portDefinitionCallback.type || callback.ERROR,
-      tag: portDefinitionCallback.tag === undefined
-        ? defaults.tagFunction
-        : portDefinitionCallback.tag,
-      name: portDefinitionCallback.name || portName + 'Finished'
-    }
-  } else if (typeof portDefinitionCallback === 'boolean') {
-    expandedCallback = portDefinitionCallback
-      ? {
-        type: callback.ERROR,
-        tag: defaults.tagFunction,
-        name: portName + 'Finished'
+export const callback = portName =>
+  createSchema({
+    root: 'type',
+    keys: {
+      type: {
+        enum: R.values(constants.callback),
+        default: constants.callback.NONE
+      },
+      tag: {
+        type: Function,
+        default: defaults.tagFunction,
+        test: fn => {
+          const result = fn([1, 2, 3])
+          return (
+            (R.has('tag', result) && R.has('rest', result))
+              || 'The callback.tag function must return an object with "tag" and "result"'
+          )
+        }
+      },
+      name: {
+        type: String,
+        default: portName + 'Finished'
       }
-      : false
-  } else if (typeof portDefinitionCallback === 'string') {
-    // Assume the callback has been set to one of the callback constants
-    expandedCallback = {
-      type: portDefinitionCallback,
-      tag: defaults.tagFunction,
-      name: portName + 'Finished'
     }
-  }
-  logger.debug(`Expanded callback for port ${portName} from`, portDefinitionCallback, 'to', expandedCallback)
-  return expandedCallback
-}
+  })
 
 // Export some additional functions for testing
 export const TESTING_USE_ONLY = {
