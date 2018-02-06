9310bc848014d5b3a6cf2ad748d8d1da70b19dcf
SidneyNemzer <sidney@ucode.com>
2018-02-05 18:00:11 -0800

    Better warning when empty port sends data

--- src/js/defaults.js
+++ src/js/defaults.js
@@ -1,5 +1,5 @@
 export const emptyPortListener = name => data =>
-  console.warn(`The empty port "${name}" sent data to JavaScript:`, data)
+  console.warn(`A port that you didn't make a lister for, "${name}", sent data to JavaScript:`, data)
 
 export const tagFunction = args => ({
   tag: args[0],
